845547f167f7cf92575f826566acef86ff526698
khomyakdi <khomyakdi@gmail.com>
2024-07-10 19:14:13 +0300

    show countries list in group

--- src/components/groups/Groups.js
+++ src/components/groups/Groups.js
@@ -2,6 +2,7 @@ import './Groups.css';
 import { useContext } from 'react';
 import GroupsContext from '../../GroupsContext';
 import NewGroup from "./NewGroup";
+import { countries } from '../../data/countries';
 
 const Groups = () => {
   const {
@@ -13,41 +14,41 @@ const Groups = () => {
   } = useContext(GroupsContext);
 
   const onGroupCreate = group => {
-      createGroup(group);
+    createGroup(group);
   }
-    
+
   const onChange = e => {
     setCurrentGroup(e.target.value);
   }
 
   return (
-      <div>
-         <NewGroup onGroupCreate={onGroupCreate} />
-         {Object.values(groups).map(group => (
-            <div key={group.name} className="group-item">
-              <div>{group.name}</div>
-              <div className="color">
-                <div style={{backgroundColor: group.color}} />
-              </div>
-              <button type="button" onClick={e => deleteGroup(group.name)}>
-                Remove
-              </button>
-              <div>
-                {group.codes.join(', ')}
-              </div>
-            </div>
-         ))}
-        <div>
-          <select onChange={onChange}>
-            {Object.values(groups).map(group => (
-              <option key={group.name} value={group.name}>{group.name}</option>
-            ))}
-          </select>
+    <div>
+      <NewGroup onGroupCreate={onGroupCreate} />
+      {Object.values(groups).map(group => (
+        <div key={group.name} className="group-item">
+          <div>{group.name}</div>
           <div className="color">
-            <div style={{backgroundColor: groups[currentGroup]?.color || ''}} />
+            <div style={{ backgroundColor: group.color }} />
+          </div>
+          <button type="button" onClick={e => deleteGroup(group.name)}>
+            Remove
+          </button>
+          <div>
+            {group.codes.map(code => countries.find(c => c.id === code)?.title || code).join(', ')}
           </div>
         </div>
+      ))}
+      <div>
+        <select onChange={onChange}>
+          {Object.values(groups).map(group => (
+            <option key={group.name} value={group.name}>{group.name}</option>
+          ))}
+        </select>
+        <div className="color">
+          <div style={{ backgroundColor: groups[currentGroup]?.color || '' }} />
+        </div>
       </div>
+    </div>
   );
 };
 
